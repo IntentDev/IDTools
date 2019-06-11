06e818fed5c830354087529b260071eb4f5f7565
Keith Lostracco <keith@the-summit.ca>
2019-06-10 20:30:45 -0700

    cleaned up internal examples

--- source/test_scripts/ExtDataTestExt1.py
+++ source/test_scripts/ExtDataTestExt1.py
@@ -16,15 +16,6 @@ class ExtDataTestExt():
 
 		self.ownerComp = ownerComp
 		
-		
-		# filter attributes so they aren't added to attrDict 
-		#self.__FILTER_GET_ATTR__ = ('C', 'D', 'Floatpar')
-		self.__FILTER_GET_ATTR__ = ('C', 'D')
-
-		# filter attributes so they aren't set to object when 
-		# retrieved from attrDict 
-		self.__FILTER_SET_ATTR__ = ('B')
-		
 		self.A = 0
 		self.B = 0
 		self.C = 1
