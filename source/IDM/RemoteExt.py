db5d32662bc60f8c0b3239441c01e3f927827966
keithlostracco <keith@IntentDev.io>
2019-08-19 06:26:54 -0700

    cleanup remote extension and added example

--- source/IDM/RemoteExt.py
+++ source/IDM/RemoteExt.py
@@ -4,61 +4,38 @@ Parps = op.IDM.op('ParProperties').module
 class RemoteExt:
 
 	def __init__(self, ownerComp):
-
 		self.ownerComp = ownerComp
 		self.tcpip = ownerComp.op('tcpip')
 		self.sync = ownerComp.op('sync')
 
 		Parps.parProperties(self, parCallbacksDAT=self.ownerComp.op('ParCallbacks'))
-
-		self.remoteFuncs = [self.setAttr, self.setPar, self.getAttr]
+		self.remoteFuncs = [self.setAttr, self.setPar, self.getAttr, self.execFunc]
 		self.connectionModes = ['client', 'server']
-
-		self._callID = -1
-		self._callList = []
 
 	# Call to Call function, set attribute or set parameter on remote component
 	##########################################
 	def SetAttr(self, comp, attribute, value):
-		self.sendData([0, comp.path, attribute, value])
+		args = pickle.dumps([0, comp.path, attribute, value])
+		self.tcpip.sendBytes(args)
 
 	def SetPar(self, comp, attribute, value):
-		self.sendData([1, comp.path, attribute, value])
+		args = pickle.dumps([1, comp.path, attribute, value])
+		self.tcpip.sendBytes(args)
 
-	def GetAttr(self, comp, attribute, *args, **kwargs):
-		self.sendData([2, comp.path, attribute, args, kwargs])
+	def GetAttr(self, comp, attribute, *args, **kwargs):	
+		data = pickle.dumps([2, comp.path, attribute, args, kwargs])
+		self.tcpip.sendBytes(data)
 
-	# Send
-	##########################################
-	def sendData(self, data):
-
-		if self.Remotecallsync:
-			bytes_ = pickle.dumps(data.append(callID))
-			self.sync.Remotecallid = self.callID
-
-		else:
-			bytes_ = pickle.dumps(datacallID)
-	
-		self.tcpip.sendBytes(bytes_)	
+	def ExecFunc(self, func, *args, **kwargs):	
+		data = pickle.dumps([3, func, args, kwargs])
+		self.tcpip.sendBytes(data)
 
 	# Receive
 	##########################################
-	def ReceiveBytes(self, bytes_):
-		data = pickle.loads(bytes_)
-		self.callFunc(self.remoteFuncs[data[0]], data[1:])
-		#self.remoteFuncs[data[0]](data[1:])
-
-	def callFunc(self, func, data):
-		
-		if self.Syncmode != 'PRO':
-			func(data[:-1])
-
-		else:
-			data = data[:-1] 
-			callID = data[-1]
-			self.callList.append([callID, func, data])
-
-
+	def ReceiveBytes(self, bytes):
+		if not self.Isserver:
+			data = pickle.loads(bytes)
+			self.remoteFuncs[data[0]](data[1:])
 
 	def setAttr(self, data):
 		comp = op(data[0])
@@ -72,26 +49,19 @@ class RemoteExt:
 		value = data[2]
 		setattr(comp.par, attribute, value)
 
-	def getAttr(self, data):	
+	def getAttr(self, data):
 		comp = op(data[0])
 		attribute = data[1]
 		args = data[2]
 		kwargs = data[3]
 		getattr(comp, attribute)(*args, **kwargs)
 
-	@property
-	def callID(self):
-		prevId = self._callID
-		self._callID += 1
-		return self._callID
-
-	@property
-	def callList(self):
-		return self._callList
-	
-	@callList.setter
-	def callList(self, value):
-		self._callList = value
+	def execFunc(self, data):
+		func = data[0]
+		args = data[1]
+		kwargs = data[2]
+		print(func)
+		exec(func)
 
 	@property
 	def ConnectionMode(self):
